93fcf6065381f18014e486516ae7938bbd65880f
donecho.20pt@gmail.com <donecho.20pt@gmail.com>
2026-02-20 02:11:27 +0700

    unknow error

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(
 app.use(express.json());
 
 const allowedOrigins = [
-  "https://burmese-library-system-57swfqxwm-done-chos-projects.vercel.app",
+  "https://burmese-library-system.vercel.app/",
   "http://localhost:5173"
 ];
 
